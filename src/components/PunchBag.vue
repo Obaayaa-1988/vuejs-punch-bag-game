8f6931abe4a5b41c18a242722e579f537b591534
Abigail <ashbella333@gmail.com>
2022-08-09 14:11:49 +0000

    added vue functionality to the punch bag game

--- src/components/PunchBag.vue
+++ src/components/PunchBag.vue
@@ -1,31 +1,82 @@
 <template>
-<div>
+  <div>
+    <!-------Bag Image------>
+    <div id="bag" v-bind:class="{ burst: ended }"></div>
 
-<!-------Bag Image------>
-<div id="bag"></div>
-
-<!-------Bag Health------>
-<div id="bag-health">
-    <div></div>
-</div>
-<!-------Bag Controls------>
-<div id="controls">
-  <button>Punch</button>
-  <button>Restart</button>
-</div>
-
-
-</div>
-  
+    <!-------Bag Health------>
+    <div id="bag-health">
+      <div v-bind:style="{ width: health + '%' }"></div>
+    </div>
+    <!-------Bag Controls------>
+    <div id="controls">
+      <button v-on:click="throwPunch" v-show="!ended">Punch</button>
+      <button v-on:click="restartPunch">Restart</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "PunchBag"
+  name: "PunchBag",
 
-}
+  data() {
+    return {
+      health: 100,
+      ended: false,
+    };
+  },
+
+  methods: {
+    throwPunch() {
+      this.health -= 10;
+      if (this.health <= 0) {
+        this.ended = true;
+      }
+    },
+    restartPunch() {
+      this.health = 100;
+      this.ended = false;
+    },
+  },
+
+  computed: {},
+};
 </script>
 
-<style>
+<style scoped>
+#bag {
+  background: url("~@/assets/bag.png");
+  width: 200px;
+  height: 500px;
+  margin: 0 auto;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
 
+#bag.burst {
+  background: url("~@/assets/bag-burst.png");
+  background-size: cover;
+}
+
+#bag-health {
+  width: 200px;
+  border: 5px solid #000;
+  margin: 50px auto 20px auto;
+}
+
+#bag-health div {
+  height: 20px;
+  background-color: crimson;
+}
+
+#controls {
+  margin: 0 auto;
+  width: 120px;
+}
+
+/* #controls button {
+  padding: 0.7rem 2rem;
+  border: none;
+  outline: none;
+} */
 </style>
